7718f6014f26e72bba072ed662e6b46c0914c650
dost2dost <dost2dost@gmail.com>
2020-02-28 22:45:05 +0500

    second commit

---
+++ src/main/java/com/zool/spbootjpacrud/SpbootJpaCrudApplication.java
@@ -0,0 +1,13 @@
+package com.zool.spbootjpacrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbootJpaCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpbootJpaCrudApplication.class, args);
+	}
+
+}
